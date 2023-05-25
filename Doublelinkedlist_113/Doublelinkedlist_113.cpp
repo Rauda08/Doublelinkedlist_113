dc03a8cc40b527287677f6690d4b26ecd8a2f6ed
rauda08 <raudajnnh08@gmail.com>
2023-05-25 15:40:45 +0700

    membuat node baru sebagai step 1

--- Doublelinkedlist_113/Doublelinkedlist_113.cpp
+++ Doublelinkedlist_113/Doublelinkedlist_113.cpp
@@ -30,6 +30,13 @@ DoubleLinkedList::DoubleLinkList() {
 }
 
 void DoubleLinkedList::addNode() {
+    int nim;
+    string nim;
+    cout << "\nEnter the roll number of the student: ";
+    cin >> nim;
+    cout << "\nEnter the name of the student: ";
+    cin >> nm;
+    Node* newNode = new Node(); //step 1
 
 }
 
