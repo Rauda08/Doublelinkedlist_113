eecec9d07c659d2db5d3f3058899e9b4ece70bea
rauda08 <raudajnnh08@gmail.com>
2023-05-25 16:17:27 +0700

    mengembalikan nilai true

--- Doublelinkedlist_113/Doublelinkedlist_113.cpp
+++ Doublelinkedlist_113/Doublelinkedlist_113.cpp
@@ -58,22 +58,44 @@ void DoubleLinkedList::addNode() {
     Node* current = START; // step 1.a
     Node* previous = NULL; // step 1.b
     while (current->next != NULL && current->nest->noMhs < nim) // step 1.c
+    {
+        previous = current; // 1.d
+        current = current->next //1.e
+    }
 
+    if (current->next != NULL && nim == current->next->noMhs) {
+        cout << "\nDuplicate roll numbers not allowed" << endl;
+        return;
+    }
+
+    newNode->next = current->next;      // step 4
+    newNode->prev = current;            // step 5
+    if (current->next != NULL)
+        current->next->prev = newNode;  // step 6
+    current->nest = newNode;  // step 7
 }
 
-
-int main()
-{
-    std::cout << "Hello World!\n";
+bool DoubleLinkedList::search(int rollNo, Node** previous, Node** current) {
+    *previous = START; //step 1.a
+    *current = START; // step 1.b
+    while (*current != NULL && rollNo != (*current)->noMhs) { //step 1.c
+        *previous = *current; //step 1.d
+        *current = (*current)->nest; //step 1.e
+    }
+    return (*current != NULL);
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+bool DoubleLinkedList::deleteNode(int rollNo) {
+    Node* previous, * current;
+    previous = current = NULL;
+    if (search(rollNo, &previous, current) == false)
+        return false;
+    if (current-> != NULL)
+        current->next-> = previous;       // step 2
+    if (previous != NULL)
+        previous->next = current->next;   // step 3
+    else
+        START = current->next;
+    delete current; // step 4
+    return true;
+}
