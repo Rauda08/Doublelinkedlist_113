63eafb985e7ff92ab2c677eef216636e4110c10f
rauda08 <raudajnnh08@gmail.com>
2023-05-25 20:34:58 +0700

    menginput roll number dan nama

--- Doublelinkedlist_113/Doublelinkedlist_113.cpp
+++ Doublelinkedlist_113/Doublelinkedlist_113.cpp
@@ -106,4 +106,65 @@ bool DoubleLinkedList::listEmpty() {
 }
 
 void DoubleLinkedList::traverse() {
+    if (listEmpty())
+        cout << "\nList is empty" << endl;
+    else {
+        cout << "\nRecords in ascending order of roll number are:" << endl;
+        Node* currentNode = START;
+        while (currentNode != NULL) {
+            cout << currentNode->noMhs << " " << currentNode->name << endl;
+            currentNode = currentNode->next;
+        }
+    }
+}
+
+void DoubleLinkedList::revtraverse() {
+    if (listEmpty()) {
+        cout << "\nList is empty" << endl;
+    }
+
+    else {
+        cout << "\nRecords in descending order of roll number are:" << endl;
+        Node* currentNode = START;
+        while (currentNode->next != NULL)
+            currentNode = currentNode->next;
+
+        while (currentNode != NULL) {
+            cout << currentNode->noMhs << " " currentNode->name << endl;
+            currentNode = currentNode->prev;
+        }
+    }
+}
+
+void DoubleLinkedList::hapus() {
+    if (listEmpty()) {
+        cout << "\nList is empty" << endl;
+
+    }
+    cout << "\Enter the roll number of the student whose record is to be deleted: ";
+    int rollNo;
+    cin >> rollNo;
+    cout << endl;
+    if (DoubleListList::deleteNode(rollNo) == false)
+        cout << "Record not found" << endl;
+    else
+        cout << "Record with roll number " << rollNo << " deleted" << endl;
+}
+void DoubleLinkedList::searchData() {
+    if (listEmpty() == true) {
+        cout << "\nList is empty" << endl;
+
+    }
+    Node* prev, * curr;
+    prev = curr = NULL;
+    cout << "nEnter the roll number of the student whose record you want to search: ";
+    int num;
+    cin >> num;
+    if (DoubleLinkedList::search(num, &prev, &curr) == false)
+        cout << "\nRecord not found" << endl;
+    else {
+        cout << "\nRecord found" << endl;
+        cout << "\nRoll number: " << curr->noMhs << endl;
+        cout << "\nName: " << curr->name << endl;
+    }
 }
