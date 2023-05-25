d7bea3ffcc13951d6891be52e5f5009d50ff6a9e
rauda08 <raudajnnh08@gmail.com>
2023-05-25 15:53:45 +0700

    mengembalikan nilai

--- Doublelinkedlist_113/Doublelinkedlist_113.cpp
+++ Doublelinkedlist_113/Doublelinkedlist_113.cpp
@@ -49,6 +49,9 @@ void DoubleLinkedList::addNode() {
         newNode->next = START; //step 3
         if (START != NULL)
             START->prev = newNode; // step 4
+        newNode->prev = NULL; //step 5
+        START = newNode; // step 6
+        return;
     }
 
 }
