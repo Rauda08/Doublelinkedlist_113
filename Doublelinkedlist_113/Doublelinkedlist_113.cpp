2ceeae70ba01fe783ffc5ff60a06e115704ea2b9
rauda08 <raudajnnh08@gmail.com>
2023-05-25 21:02:24 +0700

    membuat blok kode yang akan di eksekusi hingga case 6

--- Doublelinkedlist_113/Doublelinkedlist_113.cpp
+++ Doublelinkedlist_113/Doublelinkedlist_113.cpp
@@ -177,10 +177,30 @@ int main() {
             cout << "1. Add a record to the list" << endl;
             cout << "2. Delete a record from the list" << endl;
             cout << "3. View all records in the ascending order of roll numbers" << endl;
-            cout << "4. View all records in the descending order of roll numbers" << endl; 
+            cout << "4. View all records in the descending order of roll numbers" << endl;
             cout << "5. Search for a record in the list" << endl;
             cout << "6. Exit" << endl;
             cout << "\nEnter your choice (1-6): ";
+            char ch;
+            cin >> ch;
+
+            switch (ch) {
+            case '1':
+                obj.addNode();
+                break;
+            case '2':
+                obj.hapus();
+                break;
+            case '3':
+                obj.traverse();
+                break;
+            case '4':
+                obj.revtraverse();
+                break;
+            case '5':
+                obj.searchdata();
+                break;
+            }
         }
     }
 }
